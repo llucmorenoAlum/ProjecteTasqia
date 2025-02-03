6d6b5eefc52f9250a77d23827d981a48fb967216
llucmorenoAlum <llucmoreno.alum@insestatut.cat>
2025-02-03 15:18:18 +0000

    icon

--- app/views/header.php
+++ app/views/header.php
@@ -1,4 +1,4 @@
 <header>
     <h1>Tasqia</h1>
-    <img src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png" alt="Perfil" width="30px">
+    <img src="https://www.citypng.com/public/uploads/preview/profile-user-round-white-icon-symbol-png-701751695033499brrbuebohc.png" alt="Perfil" width="30px">
 </header>
