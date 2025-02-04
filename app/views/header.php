6a1f22935dd2412c331ce499daeb683fe996ce2a
llucmorenoAlum <llucmoreno.alum@insestatut.cat>
2025-02-04 16:46:55 +0000

    header

--- app/views/header.php
+++ app/views/header.php
@@ -1,4 +1,4 @@
 <header>
     <h1>Tasqia</h1>
-    <img src="https://ui-avatars.com/api?rounded=true" alt="Perfil" width="30px">
+    <img src="https://ui-avatars.com/api?rounded=true&name=Lluc+Moreno" alt="Perfil" width="30px">
 </header>
