d081ae6cde9d06b0f08ac900a65ef0ac62c1850a
llucmorenoAlum <llucmoreno.alum@insestatut.cat>
2025-02-04 16:18:46 +0000

    a

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -3,7 +3,7 @@ require_once 'app/models/TasquesModel.php';
 require_once 'config/database.php';
 
 function mostrarIndex(){
-    $pdo = conectarBD();
-    $tasquesPendents = getTasquesPendents($pdo);
+    //$pdo = conectarBD();
+    //$tasquesPendents = getTasquesPendents($pdo);
     require 'app/views/index.php';
 }
