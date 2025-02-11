8fc8d9e0bb62825ac0dbc865c25a980f6094328f
llucmorenoAlum <llucmoreno.alum@insestatut.cat>
2025-02-11 19:54:18 +0000

    login

---
+++ app/controllers/loginController.php
@@ -0,0 +1,7 @@
+<?php
+    require_once 'app/models/UserModel.php';
+    require_once 'config/database.php';
+
+    function mostrarlogin(){
+        require_once 'app/views/login.php';
+    }
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
     require_once 'app/controllers/indexController.php';
-/*session_start();
+    require_once 'app/controllers/loginController.php';
+session_start();
 
 // Comprova si l'usuari ha iniciat sessió
 if (!isset($_SESSION['usuari_id'])) {
     // Redirigeix a la pàgina de login
-    require_once app/views/login.php'
+    require_once 'app/views/login.php';
 }else{
     require_once 'app/views/index.php';
-}*/
+}
 mostrarIndex();
 
 ?>
