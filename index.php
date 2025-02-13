7c8341022036a53de4883f85000787a24223a1b1
llucmorenoAlum <llucmoreno.alum@insestatut.cat>
2025-02-13 18:34:56 +0000

    styles

--- index.php
+++ index.php
@@ -1,46 +1,48 @@
 <?php
     require_once 'app/controllers/indexController.php';
     require_once 'app/controllers/loginController.php';
-session_start();
+
+    session_start();
 
 // Comprova si l'usuari ha iniciat sessió
-if (!isset($_SESSION['usuari_id'])) {
-    // Redirigeix a la pàgina de login
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST['accio']) && $_POST['accio'] === 'registre') {
-            mostrarRegistre();
-        }
-        elseif(isset($_POST['accio']) && $_POST['accio'] === 'registrarse'){
-            $usuari = htmlspecialchars($_POST['usuari']);
-            $correu = htmlspecialchars($_POST['email']);
-            $contrasenya = htmlspecialchars($_POST['contrasenya']);
-            $contrasenya2 = htmlspecialchars($_POST['contrasenya2']);
+    if (!isset($_SESSION['usuari'])) {
+        // Redirigeix a la pàgina de login
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST['accio']) && $_POST['accio'] === 'registre') {
+                mostrarRegistre();
+            }
+            elseif(isset($_POST['accio']) && $_POST['accio'] === 'registrarse'){
+                $usuari = htmlspecialchars($_POST['usuari']);
+                $correu = htmlspecialchars($_POST['email']);
+                $contrasenya = htmlspecialchars($_POST['contrasenya']);
+                $contrasenya2 = htmlspecialchars($_POST['contrasenya2']);
 
-            if(registrarUsuari($usuari, $correu, $contrasenya, $contrasenya2)){
+                if(registrarUsuari($usuari, $correu, $contrasenya, $contrasenya2)){
+                    mostrarlogin();
+                }else{
+                    $error = "Aquest correu ja està registrat";
+                    mostrarRegistre($error);
+                }
+            }
+            elseif (isset($_POST['accio']) && $_POST['accio'] === 'login') {
+                $correu = htmlspecialchars($_POST['email']);
+                $contrasenya = htmlspecialchars($_POST['contrasenya']);
+                
+                if(loginUsuari($correu, $contrasenya)){
+                    $_SESSION['usuari'];
+                    mostrarIndex();
+                }else{
+                    $error = "Inici de sessió incorrecte";
+                    mostrarlogin($error);
+                }
+            }
+            else{
                 mostrarlogin();
-            }else{
-                $error = "Aquest correu ja està registrat";
-                mostrarRegistre($error);
             }
-        }
-        elseif (isset($_POST['accio']) && $_POST['accio'] === 'login') {
-            $correu = htmlspecialchars($_POST['email']);
-            $contrasenya = htmlspecialchars($_POST['contrasenya']);
-            
-            if(loginUsuari($correu, $contrasenya)){
-                mostrarIndex();
-            }else{
-                $error = "Inici de sessió incorrecte";
-                mostrarlogin($error);
-            }
-        }
-        else{
+        }else{
             mostrarlogin();
         }
     }else{
-        mostrarlogin();
+        mostrarIndex();
     }
-}else{
-    mostrarIndex();
-}
 ?>
