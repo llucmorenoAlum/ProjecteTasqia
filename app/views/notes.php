377dfae66a61101cb83fe04e4b97d3ed27d9ca87
llucmorenoAlum <llucmoreno.alum@insestatut.cat>
2025-02-27 14:27:17 +0000

    test buttons

--- app/views/notes.php
+++ app/views/notes.php
@@ -12,19 +12,29 @@
         require_once 'app/controllers/indexController.php';
         require_once 'app/views/header.php'
     ?>
-    <main>
-        <h1>Les teves notes</h1>
-        <?php if (!empty($notes)): ?>
-            <?php foreach ($notes as $nota): ?>
-                <div class="nota">
-                    <div class="titol"><?= htmlspecialchars($nota['titol']) ?></div>
-                    <div class="data"><?= date("d/m/Y H:i", strtotime($nota['data_creacio'])) ?></div>
-                    <div class="contingut"><?= nl2br(htmlspecialchars($nota['contingut'] ?? "Sense contingut")) ?></div>
-                </div>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <p>No tens cap nota.</p>
-        <?php endif; ?>
+    <main class="notes">
+        <section>
+            <h1>Les teves notes</h1>
+        </section>
+        <section>
+            <?php if (!empty($notes)): ?>
+                <?php foreach ($notes as $nota): ?>
+                    <div class="nota">
+                        <div class="titol">
+                            <?= htmlspecialchars($nota['titol']) ?>
+                            <button class="eliminar">
+                                <img src="public/media/borrar.webp" alt="Borrar">
+                            </button>
+                        </div>
+                        <div class="data"><?= date("d/m/Y H:i", strtotime($nota['data_creacio'])) ?></div>
+                        <div class="contingut"><?= nl2br(htmlspecialchars($nota['contingut'] ?? "Sense contingut")) ?></div>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>No tens cap nota.</p>
+            <?php endif; ?>
+        </section>
+        
         <form action="index.php" method="post">
             <!-- <button name="accio" value="crearNota" class="btnCrear">+</button> -->
             <button name="accio" value="crearTasca" class="Btn">
