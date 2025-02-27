91d7a56eadfcc37f9481d397147658d8fa25ca31
llucmorenoAlum <llucmoreno.alum@insestatut.cat>
2025-02-27 20:19:07 +0000

    estils calendari

--- app/views/celendari.php
+++ app/views/celendari.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tasqia</title>
     <link rel="stylesheet" href="public/css/styles.css">
+    <link rel="stylesheet" href="public/css/calendari.css">
 </head>
 <body>
     <?php 
