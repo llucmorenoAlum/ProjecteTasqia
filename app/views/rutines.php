15531fee6d64293ec20b93f8771231a673e2af99
llucmorenoAlum <llucmoreno.alum@insestatut.cat>
2025-03-20 18:55:08 +0000

    recurrencia eliminada

--- app/views/rutines.php
+++ app/views/rutines.php
@@ -41,8 +41,6 @@
                                         <input type="hidden" name="editarRutinaNom" value="<?php echo $rutina['nom'] ?>">
                                         <input type="hidden" name="editarRutinaHora" value="<?php echo $rutina['hora'] ?>">
                                         <input type="hidden" name="editarRutinaDesc" value="<?php echo $rutina['descripcio'] ?>">
-                                        <input type="hidden" name="editarRutinaRecurrencia" value="<?php echo $rutina['recurrencia'] ?>">
-
                                         <button class="botoInvisible" type="submit">
                                             <div class="nom">
                                                 <?php echo htmlspecialchars($rutina['nom']) . ' | ' . substr(htmlspecialchars($rutina['hora']), 0, 5); ?>
