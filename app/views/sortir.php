38e54a8b44a27c361800d71efe860446a70fdd56
llucmorenoAlum <llucmoreno.alum@insestatut.cat>
2025-04-23 11:04:55 +0000

    sortir

--- app/views/sortir.php
+++ app/views/sortir.php
@@ -2,7 +2,7 @@
     <div class="sortir">
         <form action="index.php" method="post">
             <button class="botoInvisibleHeader" type="submit" name="accio" value="mostrarIndex">
-                <img  src="public/media/casa.png" alt="Sortir">
+                <img id="imgSortir" src="public/media/casa.png" alt="Sortir">
             </button>
         </form>
     </div>
